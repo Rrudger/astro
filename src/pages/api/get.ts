16e035360b7a6d6ee9400e0dcf416626ec1ef54e
Rrudger <soulo@yandex.ru>
2025-08-10 15:08:53 +0200

    db

---
+++ src/pages/api/get.ts
@@ -0,0 +1,12 @@
+import type { APIRoute } from 'astro';
+import { db, Appointment } from 'astro:db';
+
+export const GET: APIRoute = async () => {
+  const data = await db.select().from(Appointment);
+  return new Response(JSON.stringify(data), {
+    status: 200,
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
